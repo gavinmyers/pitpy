9eb72dac31ae845dedcd7518c3aa364cbad9e147
gavin <gavin@crunchbang.(none)>
2012-09-19 21:28:19 -0500

    frameworking

--- test/pit.py
+++ test/pit.py
@@ -2,11 +2,18 @@
 
 import pyglet
 import unittest
+from item import Item 
 
 class u(unittest.TestCase):
   def testTrue(self):
     self.failUnless(True)
   def testFalse(self):
     self.failIf(False)
+  def testImport(self):
+    item = Item('ID',0,0,0,0)
 
-unittest.main()
+def main():
+  unittest.main()
+
+if __name__ == "__main__":
+  main()
