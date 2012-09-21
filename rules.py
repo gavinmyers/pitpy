74c77a3930ae7fe22cb9b023ea7315f066ace23a
gavin <gavin@crunchbang.(none)>
2012-09-21 17:33:26 -0500

    updating framework

---
+++ rules.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import random
+
+class Chance:
+  @staticmethod
+  def flip(source, challenge):
+    return random.randint(0,source) > challenge
+
+  @staticmethod
+  def match(source, challenge):
+    return random.randint(0,source) == challenge
+
+  @staticmethod
+  def win(s, sc, d, dc):
+    sr = random.randint(0,s) - sc
+    dr = random.randint(0,d) - dc 
+    return sr > dr
