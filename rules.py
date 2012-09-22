d569e6928125bb42e7fd76fcb799ea922f7bf2e6
gavin <gavin@crunchbang.(none)>
2012-09-21 19:42:01 -0500

    updating framework

--- rules.py
+++ rules.py
@@ -20,14 +20,3 @@ class Chance:
   def value(s):
     return random.randint(0,s)
 
-class Interact:
-  @staticmethod
-  def attack(attacker, defender):
-    attackStr = attacker.attributes["KTE"].value 
-    defendStr = defender.attributes["DEX"].value
-    if Chance.over(attackStr,defendStr):
-      Interact.damage(defender,attacker.attribute["STR"])
-
-  @staticmethod
-  def damage(who, ammount):
-    who.health -= ammount
