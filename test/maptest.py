fc18a2b251f3a375a675a82c300e17bdc50e5daa
gavin <gavin@crunchbang.(none)>
2012-10-19 19:21:49 -0500

    map

--- test/maptest.py
+++ test/maptest.py
@@ -10,9 +10,17 @@ class u(unittest.TestCase):
     diamond = Thing()
     diamond.style = "*"
     player = Monster()
+    bigrock = Thing()
+    bigrock.height=2
+    bigrock.width=2
+    bigrock.style="#"
     m = AsciiMap(10,5)
     m.tile(2,1).contents.append(diamond)
     m.tile(2,2).contents.append(player)
+    m.tile(3,3).contents.append(bigrock)
+    m.tile(3,4).contents.append(bigrock)
+    m.tile(4,4).contents.append(bigrock)
+    m.tile(4,3).contents.append(bigrock)
     m.draw() 
 
 def main():
